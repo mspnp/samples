5afbd06d1b699a5a7e4f7b5103322a79221003bf
magrande <v-mariag@microsoft.com>
2020-03-27 11:20:09 -0300

    Added sample to showcase retry pattern for resiliency in AAF

---
+++ ResiliencyRetryPatternSample/Services/IWeatherForecasetService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RetryPatternSample.Services
+{
+ public interface IWeatherForecasetService
+ {
+  Task<IEnumerable<WeatherForecast>> GetSummaries();
+ }
+}
