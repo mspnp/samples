af324a0fc05e5c93093d6ee2645e6d2e26ee3bdf
magrande <v-magran@microsoft.com>
2020-04-28 16:30:37 -0300

    Added AZ CLI script for querying common resource limits

---
+++ OperationalExcellenceResourceLimits/QueryLimits.sh
@@ -0,0 +1,52 @@
+# This set of Azure CLI commands show how yo query limit and quotas for commonly used Networking resources, Virtual machines, SQL database and Storage Accounts
+
+# Prerequisites
+
+# Either run the commands in the Azure Cloud Shell, or by running the CLI from your computer. 
+# These commands require the Azure CLI version 2.0.32 or later. Run az --version to find the installed version. 
+# You also need to run az login to log in to Azure.
+
+az login
+
+# Set the subscription you are going to query
+
+az account set -s a012a8b0-522a-4f59-81b6-aa0361eb9387
+
+
+# This command gets the limits for networking resources in the West US location:
+
+az network list-usages --location westus --query "[].{Name: localName, limit:limit}" --out table
+
+
+# Alternatively you can query for specific resources, this command gets the limits for all the IP related resources West US:
+
+az network list-usages --location westus --query "[?contains(id, 'IP')].{Name: localName, limit:limit}" --out table
+
+
+# This command gets the limits for all the Load Balancer related resources West US:
+
+az network list-usages --location westus --query "[?contains(id, 'Balancer')].{Name: localName, limit:limit}" --out table
+
+
+# Use this command to Get all SQL limits in a given location.
+
+az SQL list-usages --location westus --query "[].{Name: name, Limit: limit}" --out table
+
+
+# Use this command in case you need to query for a specific limit
+
+az SQL list-usages --location westus --query "[?name == 'SubscriptionFreeDatabaseDaysLeft'].limit"
+
+or
+
+az sql show-usage --location westus --usage VCoreQuota
+
+
+# Use this command to get the storage accounts limit
+
+az storage account show-usage --location westus
+
+
+# Use this command to get the virtual machine limits
+
+az vm list-usage --location westus --out table
