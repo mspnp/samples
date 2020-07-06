c42b2df0ef1f7bc0452b5d8c141fa1430a8f8b9c
magrande <v-magran@microsoft.com>
2020-07-06 17:52:37 -0300

    Save session in browser storage, bug fixes, alerts

--- ReliabilitySLAEstimator/SLAEstimator/SLAEstimator/ClientApp/src/components/MainPanel.js
+++ ReliabilitySLAEstimator/SLAEstimator/SLAEstimator/ClientApp/src/components/MainPanel.js
@@ -3,13 +3,26 @@ import { Navigation } from './Navigation';
 import { SearchBar } from './SearchBar';
 import { Services } from './Services';
 import { SLAestimation } from './SLAestimation';
+import { ServiceAlert } from './ServiceAlert';
 
 export class MainPanel extends Component {
-    static displayName = MainPanel.name;
 
     constructor(props) {
         super(props);
-        this.state = { serviceCategories: [], selectedServices: [], selectedCategory: "", slaEstimation: [], slaTotal: 0, downTime: 0, filter: false, loading: true };
+
+        const slaEstimation = JSON.parse(localStorage.getItem('slaEstimation')) || [];
+        const slaTotal = this.calculateSla(slaEstimation);
+        const downTime = this.calculateDownTime(slaTotal)
+
+
+        this.state = {
+            serviceCategories: [], selectedServices: [], selectedCategory: "",
+            slaEstimation: slaEstimation,
+            slaTotal: slaTotal,
+            downTime: downTime,
+            addServiceClass: "addservice-alert-hide",
+            filter: false, loading: true
+        };
         this.selectCategory = this.selectCategory.bind(this);
         this.selectService = this.selectService.bind(this);
         this.searchTextEnter = this.searchTextEnter.bind(this);
@@ -88,12 +101,20 @@ export class MainPanel extends Component {
         const slaTotal = this.calculateSla(slaEstimation);
         const downTime = this.calculateDownTime(slaTotal)
 
+        this.setState({ addServiceClass: "addservice-alert-visible" });
+
+        setTimeout(() => {
+            this.setState({ addServiceClass: "addservice-alert-hide" });
+        }, 1000);
+
         this.setState({
             selectedService: selectedService,
             slaEstimation: slaEstimation,
             slaTotal: slaTotal,
             downTime: downTime
         });
+
+        localStorage.setItem('slaEstimation', JSON.stringify(slaEstimation));
     }
 
     deleteEstimationEntry(evt) {
@@ -112,6 +133,8 @@ export class MainPanel extends Component {
             slaTotal: slaTotal,
             downTime: downTime
         });
+
+        localStorage.setItem('slaEstimation', JSON.stringify(slaEstimation));
     }
 
     deleteAll() {
@@ -120,6 +143,8 @@ export class MainPanel extends Component {
             slaTotal: 0,
             downTime: 0
         });
+
+        localStorage.setItem('slaEstimation', JSON.stringify([]));
     }
 
     expandAll(evt) {
@@ -135,7 +160,7 @@ export class MainPanel extends Component {
     expandCollapseEstimation(evt) {
         var updownimage = evt.currentTarget;
         var divPanel = evt.currentTarget.parentElement.parentElement.parentElement.children[2];
-        divPanel.className = divPanel.className === "div-hide" ? "div-show" : "div-hide";
+        divPanel.className = divPanel.className === "div-hide" ? "estimation-layout" : "div-hide";
         updownimage.className = updownimage.className === "up-arrow" ? "down-arrow" : "up-arrow";
     }
 
@@ -154,6 +179,31 @@ export class MainPanel extends Component {
                     <div className="search-container">
                         <SearchBar onTextSearchEnter={this.searchTextEnter} onClearSearch={this.clearSearch} />
                     </div>
+                    <div>
+                        <div className="tier-label">
+                            Tier
+                        </div>
+                        <div className="tier-option-div">
+                            <select className="tier-option">
+                                <option value="1">Global</option>
+                                <option value="2">Web</option>
+                                <option value="3">Api</option>
+                                <option value="4">Data</option>
+                                <option value="5">Security</option>
+                                <option value="6">Network</option>
+                            </select>
+                        </div>
+                        <div className="regions-label">
+                            Deploy to multiple Regions
+                        </div>
+                        <div className="regions-option-div">
+                            <select className="regions-option">
+                                <option value="1">Yes</option>
+                                <option value="2">No</option>
+                            </select>
+                        </div>
+                    </div>
+                    <ServiceAlert id="serviceAlert" className={this.state.addServiceClass} />
                     <div className="layout-parent-div">
                         <div className={!this.state.filter ? "layout-div-left" : "div-hide"}>
                             <Navigation visible={!this.state.filter} dataSource={this.state.serviceCategories} selectedCategory={this.state.selectedCategory} onSelectCategory={this.selectCategory} />
