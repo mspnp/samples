46f672419a037b90de3973d18a7d7dccd8001d33
magrande <v-magran@microsoft.com>
2020-07-16 16:26:54 -0300

    Renamed resiliency to reliability

---
+++ Reliability/StorageSnapshotsSample/Blob-Snapshots.ps1
@@ -0,0 +1,30 @@
+param ($ResourceGroupName)
+
+# Create variables and setup storage account context
+
+$AccountName = "storageacctres1"
+$AccountKey = (Get-AzStorageAccountKey -Name $AccountName -ResourceGroupName $ResourceGroupName)[0].Value
+$azcontext = New-AzureStorageContext -StorageAccountName $AccountName -StorageAccountKey $AccountKey
+$localFile=".\ContentFiles\sampleBlobImage.png"
+$blobName="sampleBlob"
+$containerName = "container1"
+
+
+# Upload a local file
+
+Set-AzureStorageBlobContent -File $localFile -Container $containerName -Blob $blobName -Context $azcontext
+
+# Get a reference to the blob
+
+$Blob = Get-AzureStorageBlob -Context $azcontext -Container $ContainerName -Blob $BlobName
+
+$CloudBlockBlob = $Blob.ICloudBlob
+
+# Take a snapshot
+
+$CloudBlockBlob.Metadata["filename"] = $localFile
+
+$CloudBlockBlob.SetMetadata()
+
+$CloudBlockBlob.CreateSnapshot()
+
