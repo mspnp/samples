2c13d930ce73afa6481fe450797ed96d387c0ed9
magrande <v-magran@microsoft.com>
2020-08-14 14:52:30 -0300

    changes to use management API instead of third party ps module

--- Security/AzureSentinelSample/AzSentinelSetup.ps1
+++ Security/AzureSentinelSample/AzSentinelSetup.ps1
@@ -1,4 +1,9 @@
 param (
+    $TenantId, 
+    [Parameter(Mandatory=$true)]
+    $ClientId, 
+    [Parameter(Mandatory=$true)]
+    $ClientSecret, 
     [Parameter(Mandatory=$true)]
     $Location, 
     [Parameter(Mandatory=$true)]
@@ -21,14 +26,6 @@ Set-AzContext -SubscriptionId $SubscriptionId
 
 New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 
-# Adding AzSentinel module
-
-Install-Module -Name powershell-yaml
-
-Install-Module AzSentinel -Scope CurrentUser -Force
-
-Import-Module AzSentinel
-
 # Adding OperationalInsights module
 
 Install-Module -Name Az.OperationalInsights
@@ -37,12 +34,60 @@ Install-Module -Name Az.OperationalInsights
 
 New-AzOperationalInsightsWorkspace -Location $Location -Name $WorkspaceName -Sku Standard -ResourceGroupName $ResourceGroupName
 
-# Adding Azure Sentinel to the log analytics workspace
+# Getting Azure Analytics Workspace to which the new Azure Sentinel solution will be assigned
 
-Set-AzSentinel -SubscriptionId $SubscriptionId -WorkspaceName $WorkspaceName 
+$workspaceResult = Get-AzOperationalInsightsWorkspace -Name $WorkspaceName -ResourceGroupName $ResourceGroupName
 
-# Deploy App Gateway with WAF connected to log analytics workspace
+# Setting up body and headers for the API call
 
-Write-Host "Azure Sentiel successfully set up, creating Azure App Gateway..." 
+$RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
+
+$body = "grant_type=client_credentials&client_id=$ClientId&client_secret=$ClientSecret&resource=https://management.core.windows.net/"
+
+$Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body -ContentType 'application/x-www-form-urlencoded'
+
+
+$scbody = @{
+    'id'         = ''
+    'etag'       = ''
+    'name'       = ''
+    'type'       = ''
+    'location'   = $workspaceResult.location
+    'properties' = @{
+        'workspaceResourceId' = $workspaceResult.resourceId
+    }
+    'plan'       = @{
+        'name'          = 'Test'
+        'publisher'     = 'Microsoft'
+        'product'       = 'OMSGallery/SecurityInsights'
+        'promotionCode' = ''
+    }
+}
+
+$Headers = @{}
+
+$Headers.Add("Authorization","$($Token.token_type) "+ " " + "$($Token.access_token)")
+$Headers.Add("Content-Type","application/json")
+
+
+# Calling Management API to set up the Azure Sentinel solution
+try {
+    $uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.OperationsManagement/solutions/SecurityInsights($WorkspaceName)?api-version=2015-11-01-preview"
+
+    Invoke-webrequest -Uri $uri -Method Put -Headers $Headers -Body ($scbody | ConvertTo-Json)
+}
+catch {
+    $errorReturn = $_
+    $errorResult = ($errorReturn | ConvertFrom-Json ).error
+    Write-Error "Unable to enable Sentinel on $WorkspaceName with error message: $($errorResult.message)"
+
+    exit
+}
+
+Write-Output "Successfully enabled Sentinel on workspace: $WorkspaceName"
+
+# Deploying App Gateway with WAF connected to log analytics workspace
+
+Write-Output "Creating Azure App Gateway..." 
 
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile AppGatewayDeployment.json
--- Security/AzureSentinelSample/Readme.md
+++ Security/AzureSentinelSample/Readme.md
@@ -1,8 +1,6 @@
-## Azure Sentinel Sample
+## Azure Sentinel and WAF connector Sample
 
 In this sample you create an Azure Log Analytics workspace, which is required if you want to start collecting, analyzing, and taking action on data, and an Azure Sentinel on top of it. [Azure Sentinel](https://docs.microsoft.com/azure/sentinel/overview) is a cloud-native SIEM (security instant and event management) service that runs in Azure.
-
-This Powershell script makes use [PowerShell module for Azure Sentinel](https://github.com/wortell/AZSentinel) to setup the service. 
 
 The script also installs an Azure Application Gateway as sample (ARM template is provided in the sample and used by the PS script), after you run it, you can manually configure a diagnostics setting to connect the App Gateway to the Log Analytics Workspace installed before. Behind the scenes, you are connecting Azure Sentinel to the Azure Application Gateway’s web application firewall (WAF). This WAF protects your applications from common web vulnerabilities such as SQL injection and cross-site scripting, and lets you customize rules to reduce false positives.​
 
@@ -13,10 +11,14 @@ The script also installs an Azure Application Gateway as sample (ARM template is
  - An Azure subscription
 
 ### Instructions
+
+**Since the script acquires an access token to authorize a call to the management API you will need to provide yout Azure Tenant Id, your client Id and secret.**
+
 1) Run the powerhsell script, by using this command:
 
 ```Powershell
-.\AzSentinelSetup.ps1 -Location westus -SubscriptionId [your azure subscription Id] -ResourceGroupName [your azure resource group] -WorkspaceName [your log analytics workspace name] 
+
+.\AzSentinelSetup.ps1 -Location westus -TenantId [your azure tenant id] -ClientId [your application id] -ClientSecret [your client secret] -SubscriptionId [your azure subscription Id] -ResourceGroupName [your azure resource group] -WorkspaceName [your log analytics workspace name] 
 ```
 
 2) Inside your Application Gateway resource (In Azure Portal):
