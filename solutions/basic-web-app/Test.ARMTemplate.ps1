2b2d14abab36acf33341588de7cbfa21d57c6bef
neilpeterson <neilpeterson@outlook.com>
2021-03-29 16:54:07 -0700

    ts

--- solutions/basic-web-app/Test.ARMTemplate.ps1
+++
@@ -1,7 +0,0 @@
-# Generic file used for running ARM TTK test using Pester the Invoke-Pester command.
-
-param (
-    [Parameter()]
-    [String]$TemplatePath
-)
-Test-AzTemplate -TemplatePath $TemplatePath -Pester
