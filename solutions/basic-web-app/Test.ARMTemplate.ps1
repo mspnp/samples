0f405dc90ef15fc346455ac5969369e5a0350900
neilpeterson <neilpeterson@outlook.com>
2021-03-29 14:26:12 -0700

    test path

--- solutions/basic-web-app/Test.ARMTemplate.ps1
+++ solutions/basic-web-app/Test.ARMTemplate.ps1
@@ -1,5 +1,5 @@
 param (
     [Parameter()]
-    [String]$TemplatePath = "D:\a\1\s/solutions/basic-web-app/azuredeploy.json"
+    [String]$TemplatePath
 )
 Test-AzTemplate -TemplatePath $TemplatePath -Pester
