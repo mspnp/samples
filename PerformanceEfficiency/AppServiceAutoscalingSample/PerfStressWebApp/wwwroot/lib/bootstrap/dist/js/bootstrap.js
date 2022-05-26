eb77239d925e8b628725b6c58905c082e4641a6c
Rick Hallihan <1796255+hallihan@users.noreply.github.com>
2022-05-26 10:26:20 -0400

    Update jquery
    - Update included client-side js files from latest template
    - Fix alaunchsettings to match previous change to project name

--- PerformanceEfficiency/AppServiceAutoscalingSample/PerfStressWebApp/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ PerformanceEfficiency/AppServiceAutoscalingSample/PerfStressWebApp/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1,4 +1,4 @@
-/*!
+﻿/*!
   * Bootstrap v4.3.1 (https://getbootstrap.com/)
   * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
@@ -4433,3 +4433,4 @@
 
 }));
 //# sourceMappingURL=bootstrap.js.map
+
