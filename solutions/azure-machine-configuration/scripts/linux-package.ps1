8f73351f6759a83b5cd93743e508beb889fb9b42
John Downs <john.downs@microsoft.com>
2026-07-03 12:56:06 +1200

    Deprecate azure-machine-configuration

--- solutions/azure-machine-configuration/scripts/linux-package.ps1
+++
@@ -1,9 +0,0 @@
-# Create a package that will only audit compliance
-$params = @{
-    Name          = 'NginxInstall'
-    Configuration = './NginxInstall/localhost.mof'
-    Type          = 'AuditAndSet'
-    Version       = '1.0.0'
-    Force         = $true
-}
-New-GuestConfigurationPackage @params
--- solutions/azure-machine-configuration/scripts/windows-package.ps1
+++
@@ -1,9 +0,0 @@
-# Create a package that will only audit compliance
-$params = @{
-    Name          = 'WindowsFeatures'
-    Configuration = './windowsfeatures/localhost.mof'
-    Type          = 'AuditAndSet'
-    Version       = '1.0.0'
-    Force         = $true
-}
-New-GuestConfigurationPackage @params
